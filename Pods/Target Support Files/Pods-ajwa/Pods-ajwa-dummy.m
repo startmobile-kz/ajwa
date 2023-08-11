7ad24223d9138bd01683084545d050b12dff440e
Islam <aitemr16@gmail.com>
2023-08-11 23:28:54 +0600

    remove cached files

--- Pods/Target Support Files/Pods-ajwa/Pods-ajwa-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ajwa : NSObject
-@end
-@implementation PodsDummy_Pods_ajwa
-@end
--- Pods/Target Support Files/Pods-ajwaTests/Pods-ajwaTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ajwaTests : NSObject
-@end
-@implementation PodsDummy_Pods_ajwaTests
-@end
