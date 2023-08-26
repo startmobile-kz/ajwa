ebdaeb54885629ef61a2d1d5deed7999f5abf8e8
Damir Aliyev <damiraliyevcs@gmail.com>
2023-08-26 16:30:41 +0600

    [xxh] Создал и сконфигурировал AllPrayersView.

---
+++ ajwa/Modules/Main/CustomViews/AllPrayersView.swift
@@ -0,0 +1,40 @@
+//
+//  AllPrayersView.swift
+//  ajwa
+//
+//  Created by Damir Aliyev on 26.08.2023.
+//
+
+import UIKit
+import SnapKit
+
+final class AllPrayersView: UIView {
+    
+    // MARK: - UI
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - SetupViews
+    
+    private func setupViews() {
+        
+    }
+    
+    // MARK: - SetupConstraints
+    
+    private func setupConstraints() {
+        
+    }
+    
+}
