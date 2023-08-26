c7ba659a294dd59ef818988e5fbe2fdcbff44649
Damir Aliyev <damiraliyevcs@gmail.com>
2023-08-26 14:59:34 +0600

    [85ztrgxxh] Создал и базово сконфиг. CustomSwitch.

---
+++ ajwa/Modules/Main/CustomViews/CustomSwitch.swift
@@ -0,0 +1,40 @@
+//
+//  CustomSwitch.swift
+//  ajwa
+//
+//  Created by Damir Aliyev on 26.08.2023.
+//
+
+import SnapKit
+import UIKit
+
+final class CustomSwitch: UIView {
+    
+    // MARK: - UI
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - SetupViews
+    
+    private func setupViews() {
+        
+    }
+    
+    // MARK: - SetupConstraints
+    
+    private func setupConstraints() {
+        
+    }
+    
+}
