64be55cfc555541cac7cc444331644d00e5d9fb5
Damir Aliyev <damiraliyevcs@gmail.com>
2023-08-28 16:34:52 +0600

    [xxh] Создал и сконфигурировал MainBottomView.

---
+++ ajwa/Modules/Main/CustomViews/MainBottomView.swift
@@ -0,0 +1,39 @@
+//
+//  MainBottomView.swift
+//  ajwa
+//
+//  Created by Damir Aliyev on 28.08.2023.
+//
+
+import UIKit
+import SnapKit
+
+final class MainBottomView: UIView {
+    
+    // MARK: - UI
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - SetupViews
+    
+    private func setupViews() {
+        
+    }
+    
+    // MARK: - SetupConstraints
+    
+    private func setupConstraints() {
+        
+    }
+}
