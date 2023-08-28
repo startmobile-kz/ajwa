c577f9509a359583edd4c5bf0670a3be0ee7b274
Damir Aliyev <damiraliyevcs@gmail.com>
2023-08-28 13:07:55 +0600

    [xxh] Создал нужные UI компоненты для ячейки.

--- ajwa/Modules/Main/Cells/SacredEventCell.swift
+++ ajwa/Modules/Main/Cells/SacredEventCell.swift
@@ -14,6 +14,26 @@ final class SacredEventCell: UICollectionViewCell {
     
     // MARK: - UI
     
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    private lazy var eventNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = AppFont.regular.s12()
+        label.text = "Рамаданға"
+        return label
+    }()
+    
+    private lazy var remainingDaysLabel: UILabel = {
+        let label = UILabel()
+        label.font = AppFont.bold.s20()
+        label.textColor = AppColor.blue.uiColor
+        label.text = "92 күн қалды"
+        return label
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
@@ -25,6 +45,11 @@ final class SacredEventCell: UICollectionViewCell {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = 20
     }
     
     private func setupViews() {
