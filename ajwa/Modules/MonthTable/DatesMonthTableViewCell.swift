338568b230248ff28f3c6892ae324691f6da9d05
Adlet <adlet.zh19966@gmail.com>
2023-08-30 13:06:59 +0600

    [85ztqca21] создал две кастомные ячейки для экрана MonthTableViewController

--- ajwa/Modules/MonthTable/DatesMonthTableViewCell.swift
+++ ajwa/Modules/MonthTable/DatesMonthTableViewCell.swift
@@ -8,16 +8,65 @@
 import UIKit
 
 class DatesMonthTableViewCell: UITableViewCell {
+    
+    static let identifier = "DatesMonthTableViewCell"
+    
+    lazy var mainView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 15
+        view.backgroundColor = AppColor.white.uiColor
+        return view
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    var rowNumber = UILabel(text: "", font: .systemFont(ofSize: 12), textColor: .black)
+    var label1 = UILabel(text: "", font: .systemFont(ofSize: 12), textColor: .black)
+    var label2 = UILabel(text: "", font: .systemFont(ofSize: 12), textColor: .black)
+    var label3 = UILabel(text: "", font: .systemFont(ofSize: 12), textColor: .black)
+    var label4 = UILabel(text: "", font: .systemFont(ofSize: 12), textColor: .black)
+    var label5 = UILabel(text: "", font: .systemFont(ofSize: 12), textColor: .black)
+    var label6 = UILabel(text: "", font: .systemFont(ofSize: 12), textColor: .black)
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = AppColor.background.uiColor
+        
+        addSubview(mainView)
+        mainView.addSubview(stackView)
+        stackView.addArrangedSubview(rowNumber)
+        stackView.addArrangedSubview(label1)
+        stackView.addArrangedSubview(label2)
+        stackView.addArrangedSubview(label3)
+        stackView.addArrangedSubview(label4)
+        stackView.addArrangedSubview(label5)
+        stackView.addArrangedSubview(label6)
+        
+        mainView.snp.makeConstraints { make in
+            make.top.equalTo(snp.top).offset(5)
+            make.leading.equalTo(snp.leading).offset(5)
+            make.trailing.equalTo(snp.trailing).offset(-5)
+            make.bottom.equalTo(snp.bottom).offset(-5)
+        }
+        
+        stackView.snp.makeConstraints { make in
+            make.top.equalTo(mainView.snp.top)
+            make.leading.equalTo(mainView.snp.leading).offset(10)
+            make.trailing.equalTo(mainView.snp.trailing).offset(-10)
+            make.bottom.equalTo(mainView.snp.bottom)
+        }
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
 }
