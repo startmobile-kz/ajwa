2b161a787f4db5b0910e2949baf4056100b91599
Adlet <adlet.zh19966@gmail.com>
2023-08-30 13:08:05 +0600

    [85ztqca21] создал TopView для MonthTableViewController

--- ajwa/Modules/MonthTable/TopView.swift
+++ ajwa/Modules/MonthTable/TopView.swift
@@ -8,13 +8,76 @@
 import UIKit
 
 class TopView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    var data = ["Январь", "Февраль", "Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"]
+    var index = 5
+    
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = data[index]
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 17, weight: .bold)
+        return label
+    }()
+    
+    lazy var leftArrowButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(AppImage.leftArrow.uiImage, for: .normal)
+        button.addTarget(self, action: #selector(leftArrowButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var rightArrowButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(AppImage.rightArrow.uiImage, for: .normal)
+        button.addTarget(self, action: #selector(rightArrowButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func leftArrowButtonPressed() {
+        if index > 0 {
+            index -= 1
+            label.text = data[index]
+        }
+        print("left arrow \(index)")
+        
     }
-    */
-
+    
+    @objc func rightArrowButtonPressed() {
+        if index < (data.count - 1) {
+            index += 1
+            label.text = data[index]
+        }
+        print("right arrow \(index)")
+    }
+ 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(label)
+        addSubview(leftArrowButton)
+        addSubview(rightArrowButton)
+        backgroundColor = .white
+        layer.cornerRadius = 15
+        
+        leftArrowButton.snp.makeConstraints { make in
+            make.leading.equalTo(snp.leading).offset(10)
+            make.centerY.equalTo(snp.centerY)
+        }
+        
+        label.snp.makeConstraints { make in
+            make.centerX.equalTo(snp.centerX)
+            make.centerY.equalTo(snp.centerY)
+        }
+        
+        rightArrowButton.snp.makeConstraints { make in
+            make.trailing.equalTo(snp.trailing).offset(-10)
+            make.centerY.equalTo(snp.centerY)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
