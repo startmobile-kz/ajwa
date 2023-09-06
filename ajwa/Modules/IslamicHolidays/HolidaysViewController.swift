ffc6e5ea442a7130e912c7787bd0adf4229f0a2a
Damir Aliyev <damiraliyevcs@gmail.com>
2023-09-06 14:14:31 +0600

    [858] Базово сконфигурировал коллекцию.

--- ajwa/Modules/IslamicHolidays/HolidaysViewController.swift
+++ ajwa/Modules/IslamicHolidays/HolidaysViewController.swift
@@ -18,6 +18,13 @@ class HolidaysViewController: UIViewController, HolidaysDisplayLogic {
     
     // MARK: - UI
     
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        return collectionView
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -31,13 +38,47 @@ class HolidaysViewController: UIViewController, HolidaysDisplayLogic {
     
     private func setupViews() {
         view.backgroundColor = AppColor.background.uiColor
-    }
-    
-    private func setupConstraints() {
         
+        view.addSubview(collectionView)
     }
     
     // MARK: - SetupConstraints
+    
+    private func setupConstraints() {
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.bottom.equalToSuperview()
+        }
+    }
+    
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        // Item
+        
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalHeight(1)
+            )
+        )
+        
+        // Group
+        
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .absolute(126)
+            ),
+            subitems: [item]
+        )
+        
+        // Section
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return UICollectionViewCompositionalLayout(section: section)
+    }
     
     // MARK: Do something
     
@@ -49,4 +90,23 @@ class HolidaysViewController: UIViewController, HolidaysDisplayLogic {
     func displaySomething(viewModel: Holidays.Something.ViewModel) {
         //nameTextField.text = viewModel.name
     }
+}
+
+
+// MARK: - UICollectionViewDataSource methods
+
+extension HolidaysViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+
+// MARK: - UICollectionViewDelegate methods
+
+extension HolidaysViewController: UICollectionViewDelegate {
+    
 }
