51d280815d25ed07ee98aa5e20d489599330ffb9
Damir Aliyev <damiraliyevcs@gmail.com>
2023-09-06 14:05:01 +0600

    [858] Базово настроил HolidaysVC.

--- ajwa/Modules/IslamicHolidays/HolidaysViewController.swift
+++ ajwa/Modules/IslamicHolidays/HolidaysViewController.swift
@@ -16,18 +16,30 @@ class HolidaysViewController: UIViewController, HolidaysDisplayLogic {
     var interactor: HolidaysBusinessLogic?
     var router: (NSObjectProtocol & HolidaysRoutingLogic & HolidaysDataPassing)?
     
-    
+    // MARK: - UI
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        doSomething()
+        
+        setupViews()
+        setupConstraints()
     }
     
-    // MARK: Do something
+    // MARK: - SetupViews
     
-    //@IBOutlet weak var nameTextField: UITextField!
+    private func setupViews() {
+        view.backgroundColor = AppColor.background.uiColor
+    }
+    
+    private func setupConstraints() {
+        
+    }
+    
+    // MARK: - SetupConstraints
+    
+    // MARK: Do something
     
     func doSomething() {
         let request = Holidays.Something.Request()
--- ajwa/SceneDelegate.swift
+++ ajwa/SceneDelegate.swift
@@ -14,15 +14,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: UIViewController())
-        let namazController = NamazViewController()
-        namazController.view.backgroundColor = .white
-        self.window?.rootViewController = navigationController
-        self.window?.makeKeyAndVisible()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            navigationController.pushViewController(namazController, animated: true)
-        }
-        guard let _ = (scene as? UIWindowScene) else { return }
+        let holidaysViewController = HolidaysViewController()
+        HolidaysConfigurator.shared.configure(
+            viewController: holidaysViewController
+        )
+        window?.rootViewController = holidaysViewController
+        window?.makeKeyAndVisible()
     }
 
 }
